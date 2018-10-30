0fe8ffd053c589793f7bd59a890ecbf081a6494e
Matteo Cominetti <matteo@cominetti.org>
2018-10-30 18:36:27 +0000

    init receiver ui

---
+++ SpeckleClientUI.Test/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeckleClientUI.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            receiver.DataContext = new List<ReceiverViewModel>
+            {
+                new ReceiverViewModel
+                {
+                    StreamName="TEST",
+                    StreamId="123",
+                },
+                                new ReceiverViewModel
+                {
+                    StreamName="AAAA",
+                    StreamId="sdfsdf",
+                },
+            };
+        }
+    }
+}
---
+++ SpeckleClientUI/SpeckleExpander.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SpeckleClientUI
+{
+    public class SpeckleExpander : Expander
+    {
+
+        public static DependencyProperty StreamIdProperty =
+            DependencyProperty.Register(
+                "StreamId",
+                typeof(string),
+                typeof(SpeckleExpander));
+
+      
+
+        static SpeckleExpander()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(SpeckleExpander),
+                new FrameworkPropertyMetadata(typeof(SpeckleExpander)));
+        }
+
+
+       public string StreamId
+        {
+            get { return (string)GetValue(StreamIdProperty); }
+            set { SetValue(StreamIdProperty, value); }
+        }
+
+
+
+    }
+}
