75083be87889c1f8181c369304537931c9bc9895
matteo.cominetti <matteo.cominetti@arup.com>
2018-11-01 11:44:49 +0000

    Added dropdowns for accounts and streams

--- SpeckleClientUI/ReceiversUi.xaml.cs
+++ SpeckleClientUI/ReceiversUi.xaml.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using SpeckleClientUI;
+using System.Windows.Threading;
 using SpeckleCore;
 
 namespace SpeckleClientUI
@@ -23,33 +15,35 @@ namespace SpeckleClientUI
     /// </summary>
     public partial class ReceiversUi : UserControl
     {
-        ObservableCollection<Receiver> _receivers = new ObservableCollection<Receiver>();
+        private ObservableCollection<Receiver> _receivers = new ObservableCollection<Receiver>();
+        private ObservableCollection<Account> _accounts = new ObservableCollection<Account>();
         public delegate void ReceivedData(List<SpeckleObject> objs);
         public event ReceivedData OnUpdateGlobal;
 
         public ReceiversUi()
         {
             InitializeComponent();
+            LocalContext.Init();
+            _accounts = new ObservableCollection<Account>(LocalContext.GetAllAccounts());
+            AccountsComboBox.ItemsSource = _accounts;
+            if (_accounts.Any(x => x.IsDefault))
+            {
+                AccountsComboBox.SelectedIndex = _accounts.IndexOf(_accounts.First(x => x.IsDefault));
+            }
+
             ReceiverItemsControl.ItemsSource = _receivers;
         }
 
         private void NewReceiverClick(object sender, RoutedEventArgs e)
         {
-            var myForm = new SpecklePopup.MainWindow();
-            //TODO: fix this it's crashing revit
-            //myForm.Owner = Application.Current.MainWindow;
 
-            //if default account exists form is closed automatically
-            if (!myForm.HasDefaultAccount)
-                myForm.ShowDialog();
-            if (myForm.restApi != null && myForm.apitoken != null)
-            {
-                _receivers.Add(new Receiver(StreamId.Text, myForm.selectedServer, myForm.restApi, myForm.apitoken, myForm.selectedEmail));
-            }
-            else
-            {
-                MessageBox.Show("Account selection failed.");
-            }
+            if (StreamsComboBox.SelectedIndex == -1 || AccountsComboBox.SelectedIndex==-1)
+                return;
+
+            var stream = StreamsComboBox.SelectedItem as SpeckleStream;
+            var account = _accounts[AccountsComboBox.SelectedIndex];
+
+            _receivers.Add(new Receiver(stream.StreamId, account.ServerName, account.RestApi, account.Token, account.Email));
         }
 
         #region commands
@@ -58,35 +52,64 @@ namespace SpeckleClientUI
         {
             var r = e.Parameter as Receiver;
 
-           var objs = r.UpdateGlobal();
+            var objs = r.UpdateGlobal();
             OnUpdateGlobal(objs);
         }
 
-      
+
 
         private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
 
-            if (_receivers.Any(x=>x.Transmitting))
+            if (_receivers.Any(x => x.Transmitting))
+            {
                 e.CanExecute = false;
+            }
             else
+            {
                 e.CanExecute = true;
+            }
         }
 
         #endregion
 
         private void PasteClick(object sender, RoutedEventArgs e)
         {
-            var c = Clipboard.GetText();
-            if (c.Length>10)
-                c = c.Substring(0, 10);
-            StreamId.Text = c;
+            //var c = Clipboard.GetText();
+            //if (c.Length>10)
+            //    c = c.Substring(0, 10);
+            //StreamId.Text = c;
         }
 
         private void AccountsClick(object sender, RoutedEventArgs e)
         {
             var myForm = new SpecklePopup.MainWindow(false);
             myForm.ShowDialog();
+        }
+
+        private void AccountsComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AccountsComboBox.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            var client = new SpeckleApiClient();
+            var account = _accounts[AccountsComboBox.SelectedIndex];
+
+            client.BaseUrl = account.RestApi;
+            client.AuthToken = account.Token;
+            client.StreamsGetAllAsync().ContinueWith(tsk =>
+            {
+                Application.Current.Dispatcher.BeginInvoke(
+                  DispatcherPriority.Background,
+                  new Action(() =>
+                  {
+                      StreamsComboBox.ItemsSource = tsk.Result.Resources.ToList();
+                  }));
+
+            });
+
         }
     }
 
